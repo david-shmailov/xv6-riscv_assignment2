89bf9e719d6a71416a7c6173698324cc752416af
David <shmailov.david@gmail.com>
2022-05-05 17:30:04 +0300

    dummy

--- kernel/linked_list.c
+++ kernel/linked_list.c
@@ -3,6 +3,10 @@
 //
 
 #include "linked_list.h"
+
+sleeping_ls = init_linked_list()
+
+
  struct node init_node(uint proc_index){
     struct node node;
     node.proc_index =proc_index;
@@ -13,7 +17,9 @@
 
 struct list init_linked_list(){
     struct list ls;
-    ls.head = NULL;
+    //ls.head = NULL;
+    struct node dummy = init_node(0); // dummy
+    ls.head = dummy;
     return ls;
 }
 
@@ -30,6 +36,11 @@ void add(struct list ls, struct node node){
 
 uint pop(struct list ls){
     struct node curr = ls.head;
+    if curr.next == NULL{
+        //some exception?
+    }else {
+        curr = curr.next; // skip dummy
+    }
     // todo pseudo code
     proc[curr.proc_index].acquire()
     ls.head = curr.next;
